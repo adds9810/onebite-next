33e7f6e9a5ae5682417bed8b08f1899a7962544b
김지혜 <ria9810@gmail.com>
2024-09-24 23:21:23 +0900

    테스트용 데이터 서버 세팅

---
+++ onebite-movie/src/pages/api/hello.ts
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ name: "John Doe" });
+  // 상태코드 200 정상적으로 처리 되었음을 의미
+}
--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,13 +1,38 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 // App : 리액트 앱 컴포넌트(루트 컴포넌트)와 동일, 모든 페이지의 부모 역할을 함
 // Component : 페이지 역할
 // pageProps : Component한테 전달될 페이지의 Props들을 모두 객체로 보관한 것
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const onclickButton = () => {
+    router.push("/test");
+    // router 주로사용 메서드
+    // replace
+    // back 뒤로가기
+  };
+  useEffect(() => {
+    router.prefetch("/test"); // 프리패칭하고자 하는 경로 입력
+  }, []); // 마운트 되었을 때 한번만 실행할 예정이므로 빈 객체를 둠
   return (
     <>
-      <header>header</header>
+      <header>
+        <Link href={"/"}>index</Link>
+        &nbsp;
+        <Link href={"/search"} prefetch={false}>
+          search
+        </Link>
+        &nbsp;
+        <Link href={"/book/1"}>book/1</Link>
+        <div>
+          <button onClick={onclickButton}>/test 페이지로 이동</button>
+        </div>
+      </header>
+
       <Component {...pageProps} />
     </>
   );
