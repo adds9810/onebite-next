e3b446a5296c23b810599fa720570739a5c88138
김지혜 <ria9810@gmail.com>
2024-11-11 00:57:10 +0900

    ssg로 설정 변경 중

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -4,13 +4,15 @@ import style from "./index.module.css";
 import { ReactNode, useEffect } from "react";
 // import books from "@/mock/book.json";
 import BookItem from "./component/book-item";
-import { InferGetServerSidePropsType } from "next";
+import { InferGetServerSidePropsType, InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-boots";
 import fetchRandomBooks from "@/lib/fecth-random.books";
 
 // getServerSideProps 약속된 메서드를 추가하면 해당 패이지는 ssr로 실행됨
 // 딱 한번만 서버측에서 실행됨
-export const getServerSideProps = async () => {
+// getStaticProps ssg 정적 페이지 실행
+// 개발모드에서는 ssg, ssr 모두 새로고침 하면 바로 실행되게 되어 있어서 npm run build로 실행해서 확인해봐야 함
+export const getStaticProps = async () => {
   // 컴포넌트 보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
   // 데이터 한번에 받기
   const [allBooks, recoBooks] = await Promise.all([
@@ -24,10 +26,11 @@ export const getServerSideProps = async () => {
 
 // any 치트키 타입
 // InferGetServerSidePropsType getServerSideProps의 타입을 추론해줌
+// InferGetStaticPropsType getStaticProps
 export default function Home({
   allBooks,
   recoBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   console.log(allBooks);
   useEffect(() => {
     console.log(window);
