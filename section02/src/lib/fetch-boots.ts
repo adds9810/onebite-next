8b9e6e824cace152e877c971f7939ae680f82bd2
김지혜 <ria9810@gmail.com>
2024-11-18 01:16:00 +0900

    section02 vercel 연결 section03 세팅

--- section02/src/lib/fetch-boots.ts
+++ section02/src/lib/fetch-boots.ts
@@ -2,7 +2,7 @@ import { BookData } from "@/types";
 
 // Promise<BookData[]> 비동기래 책 데이터 반환
 export default async function fetchBooks(q?: string): Promise<BookData[]> {
-  let url = `http://localhost:12345/book`;
+  let url = `https://onebite-books-server-iota-two.vercel.app/book`;
   if (q) {
     url += `/search?q=${q}`;
   }
